4e48e24af55953cf777ecf29e512a96e12d2df78
Muḥend Belqasem <belkacem77@gmail.com>
2018-01-16 21:20:01 +0100

    Lemmatisation aoriste futur deuxième personnes
    Lemmatisation aoriste futur deuxième personnes

---
+++ Lammatization/lemmatisation-verbe/lemmatisation-verbe-aoriste(futur)2.py
@@ -0,0 +1,94 @@
+#-------------------------------------------------------------------------------
+# Name:        lemmatize aoriste futur
+# Purpose:     Lemmatization for Kabyle
+#
+# Author:      Muḥend Belqasem
+#
+# Created:     15/01/2018
+# Copyright:   (c) Belkacem Mohammed 2018
+# Licence:     <MIT>
+#-------------------------------------------------------------------------------
+def sekyed_asekkil (asekkil,tighra):
+    #yella=False
+    for i in tighra:
+        if asekkil==i:
+            return True
+
+    return False
+
+def amasal (azrir):
+    amasal=''
+    for i in azrir:
+        if i =='e':
+            amasal=amasal+'e'
+        else:
+            amasal = amasal+'c'
+    return amasal
+
+
+voyelles=['a','e','i','u']
+imasalen=[{'cecec',0}]# ajouter ici les formes issues des différentes flexions après avoir former le radical sans suppression de la lettre e issue de la flexion verbiale
+for line in open("C:/tal/corpus-aoriste2.txt"):
+    #Aoriste 1 ière personne
+        i = line.split()
+        i= i[0]
+        a=""
+        #print i
+
+        if (i.endswith('iḍ') or i.endswith('uḍ') ) and ((i.startswith('ta') or i.startswith('te'))):
+            if (i.startswith('ta')):
+             a=i[1:len(i)-3]
+            else:
+                if i.startswith('te'):
+                    a=i[2:len(i)-3]
+
+
+
+            #print i.decode('utf8')
+        else:
+            if (i.endswith('iḍ')) and (i.startswith('t')):
+                        a=i[1:len(i)-3]
+
+            if (i.endswith('eḍ')) and (i.startswith('te')  ):
+             a=i[2:len(i)-3].decode('utf8')
+             #print a
+            if i.startswith('ta') or i.startswith('te'):
+             if i.startswith('ta'):
+                  a=i[1:len(i)-4].decode('utf8')
+             if i.startswith('te'):
+                a=i[2:len(i)-4].decode('utf8')
+
+             #print 'ici', a
+             if ( a[len(a)-2] not in voyelles): # Si l'avant dernière lettre n'est pas une voyelle
+                if (a[len(a)-2]<>a[len(a)-1]): #Si les deux dernière lettre ne sont pas les même
+                 #print a[0:len(a)-1], a[len(a)-2], a[len(a)-1]
+                 a=a[0:len(a)-1]+'e'+a[len(a)-1] # on insère alors un e
+                 #print a , 'ici'
+                else:
+                    if (len (a)<=2 and a[len(a)-1] not in voyelles):
+                          a='e'+a[len(a)-2:len(a)] # si plus de deux consonnes
+                          #print a,'ici'
+
+            if (len (a)<=2 ):
+
+                          #print a,'ici',len(a),a[len(a)-2:len(a)-1]
+                          a='e'+a[len(a)-2:len(a)] # si plus de deux consonnes
+                          #print a,'ici', a[len(a)-1]
+
+
+        # traietement des cas de déplacement de e
+        if (amasal(a)=='cecec') :
+            a=a[0]+a[2:len(a)]
+        if (amasal(a)=='ccccecec'):
+            a=a[0:4]+a[5:len(a)]
+        if (amasal(a)=='cccecec'):
+            a=a[0:3]+a[4:len(a)]
+        if (amasal(a)=='ccecec'):
+            a=a[0:1]+'e'+a[1:2]+a[len(a)-3:len(a)]
+        if (amasal(a)=='ccececec'):
+            #print a
+            a=a[0:4]+a[len(a)-3:len(a)]
+
+
+        print i.decode('utf8'), '->', a
+        a=""
