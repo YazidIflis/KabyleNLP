769ff78dbeff023fe2a109c92ba014bdddfa1aea
Muḥend Belqasem <belkacem77@gmail.com>
2018-01-18 11:18:27 +0100

    Update lemmatisation-verbe-aoriste(futur)3.py

--- Lammatization/lemmatisation-verbe/lemmatisation-verbe-aoriste(futur)3.py
+++ Lammatization/lemmatisation-verbe/lemmatisation-verbe-aoriste(futur)3.py
@@ -35,17 +35,18 @@ for line in open("C:/tal/corpus-aoriste3.txt"):
         a=""
         #print i
 
-        if i.startswith('i') or i.startswith('ya') or i.startswith('yi'):
-             a=i[1:len(i)].decode('utf8')
+        if i.startswith('i')  or (i.startswith('t') and not i.startswith('te')) or i.startswith('ti') or i.startswith('ta') or i.startswith('yi') or i.startswith('ya') or i.startswith('yu')  :
+                a=i[1:len(i)].decode('utf8')
+
 
 
 
             #print i.decode('utf8')
         else:
-            if i.startswith('ye'):
+            if i.startswith('ye') or i.startswith('te'):
                 a=i[2:len(i)].decode('utf8')
-            #print a, 'ici'
-            if (len (a)<=2)  :
+
+        if (len (a) <=2 and len(a)>0)  and a[0] not in voyelles and a[len(a)-1] not in voyelles  :
                 a='e'+a[len(a)-2:len(a)] # si plus de deux consonnes
 
 
